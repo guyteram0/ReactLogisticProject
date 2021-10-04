0adabe2d4ddb9be29f37da02472d425cd5c0c647
GuyTeram <guyteram0@gmail.com>
2021-10-04 15:42:24 +0300

    Updates

---
+++ precache-manifest.dafc311cf9bd5a36f82f02806651decb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f1058d72b38ffd23f5dd67f9d3546c",
+    "url": "/ReactLogisticProject/index.html"
+  },
+  {
+    "revision": "7bb3022af424b4c2dab4",
+    "url": "/ReactLogisticProject/static/css/main.cd0f6bb2.chunk.css"
+  },
+  {
+    "revision": "5d444599dbd9c91cfb1b",
+    "url": "/ReactLogisticProject/static/js/2.957757a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactLogisticProject/static/js/2.957757a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb3022af424b4c2dab4",
+    "url": "/ReactLogisticProject/static/js/main.ec437fae.chunk.js"
+  },
+  {
+    "revision": "7ef0efe5d90bb1285b2b",
+    "url": "/ReactLogisticProject/static/js/runtime-main.cc694613.js"
+  }
+]);
